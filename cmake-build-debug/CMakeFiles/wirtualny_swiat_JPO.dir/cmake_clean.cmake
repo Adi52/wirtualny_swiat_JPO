f0b902160f0e935e4134ec65b32e811ab615ada2
Adi52 <adimr52@gmail.com>
2020-12-06 02:36:35 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/wirtualny_swiat_JPO.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wirtualny_swiat_JPO.dir/main.cpp.obj"
+  "wirtualny_swiat_JPO.exe"
+  "wirtualny_swiat_JPO.exe.manifest"
+  "wirtualny_swiat_JPO.lib"
+  "wirtualny_swiat_JPO.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wirtualny_swiat_JPO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/wirtualny_swiat_JPO.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for wirtualny_swiat_JPO.
+# This may be replaced when dependencies are built.
